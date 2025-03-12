432211de2ce98fe3121874f9e5c157de87d8b4f6
ChicoChen <chin2839211@gmail.com>
2025-03-12 17:53:08 +0800

    add meta-address file address.h

--- include/simple_commands.h
+++ include/simple_commands.h
@@ -3,15 +3,15 @@
 
 #define CMD_BUFFER_SIZE 64
 
-
 typedef struct{
     char *name;
     int (*func)(void*);
     char* description;
 } Command;
 
+extern Command commands[];
+
 int cmd_help(void* args);
 int hello_world(void* args);
 
-extern Command commands[];
 #endif
