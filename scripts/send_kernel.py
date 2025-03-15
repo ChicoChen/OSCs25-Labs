a0ca23ae307a700a66738db5e22ad56335fc31c6
ChicoChen <chin2839211@gmail.com>
2025-03-15 20:39:07 +0800

    add cat command

--- scripts/send_kernel.py
+++ scripts/send_kernel.py
@@ -8,16 +8,20 @@ kernel_size = os.path.getsize(kernel_img)
 tag = "KIMG"
 hex_tag = int.from_bytes(tag.encode(), "big")
 header = struct.pack('>II', hex_tag, kernel_size)
-
-interval = 8e-5
+interval = 8e-4
+print(f"size: {kernel_size}")
 
 with open('/dev/ttyUSB0', 'wb', buffering= 0) as target,\
     open(kernel_img, 'rb', buffering=0) as image:
     target.write(header)
-    time.sleep(3)
+    print("header sent!")
+    time.sleep(1)
 
+    print("sending kernel...")
     while kernel_size > 0:
         target.write(image.read(1))
         kernel_size -= 1
         time.sleep(interval)
+    target.flush()
+    print("All Done!")
     time.sleep(1)
