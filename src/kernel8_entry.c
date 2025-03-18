947e8de46213a593f82d50ad5dcceb29a8a8b2e8
ChicoChen <chin2839211@gmail.com>
2025-03-18 12:34:28 +0800

    modify loader to pass _dtb_addr to kernel

--- src/kernel8_entry.c
+++ src/kernel8_entry.c
@@ -1,8 +1,15 @@
 #include "mini_uart.h"
 #include "simple_shell.h"
+#include "devicetree/dtb.h"
+#include "basic_type.h"
+
+#include "str_utils.h"
+extern void *_dtb_addr;
 
 void kernel_entry(){
     init_uart();
+    send_line("----------");
+    dtb_parser(find_initramfs, (addr_t)_dtb_addr);
     send_line("hello world");
     simple_shell();
 }
