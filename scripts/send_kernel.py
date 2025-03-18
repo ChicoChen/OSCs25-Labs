3d3dd3947790bc74ac4f9a0e25b3eba12118b1e6
ChicoChen <chin2839211@gmail.com>
2025-03-18 17:10:25 +0800

    modify send_kernel.py to run on window OS

--- scripts/send_kernel.py
+++ scripts/send_kernel.py
@@ -1,7 +1,8 @@
+import serial
 import struct
 import os
 import time
-
+#import argparse
 kernel_img = "../build/kernel8.img"
 kernel_size = os.path.getsize(kernel_img)
 
@@ -11,18 +12,20 @@ header = struct.pack('>II', hex_tag, kernel_size)
 interval = 8e-5
 print(f"size: {kernel_size}")
 print(header)
-
-with open('/dev/ttyUSB0', 'wb', buffering= 0) as target,\
-    open(kernel_img, 'rb', buffering=0) as image:
-    target.write(header)
+ser = serial.Serial('/dev/ttyUSB0', baudrate=115200, timeout = 1)
+# open('/dev/ttyUSB0', 'wb', buffering= 0) as target
+with open(kernel_img, 'rb', buffering=0) as image:
+    ser.write(header)
     print("header sent!")
     time.sleep(1)
 
     print("sending kernel...")
     while kernel_size > 0:
-        target.write(image.read(1))
+        ser.write(image.read(1))
         kernel_size -= 1
         time.sleep(interval)
-    target.flush()
+    ser.flush()
     print("All Done!")
     time.sleep(1)
+
+ser.close()
